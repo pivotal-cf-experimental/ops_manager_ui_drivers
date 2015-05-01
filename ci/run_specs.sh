0c0afe0630575f97cb4b4a64c505a468c1b12126
Aram Price <aprice@pivotal.io>
2015-05-01 11:05:42 -0700

    Fix docker invocation to have correct path

--- ci/run_specs.sh
+++ ci/run_specs.sh
@@ -5,7 +5,7 @@ echo "-----> Running script: $0"
 
 docker run \
   --rm=true \
-  --volume=${PWD}:/vm_shepherd \
+  --volume=${PWD}:/ops_manager_ui_drivers \
   --workdir=/ops_manager_ui_drivers \
   ${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME} \
   /bin/sh -c 'bundle && bundle exec rake -t'
