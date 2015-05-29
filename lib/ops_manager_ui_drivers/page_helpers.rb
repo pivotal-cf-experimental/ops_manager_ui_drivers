e85508237b15435f2ded8be937c2de844561ba57
Abhi Hiremagalur and Ryan Spore <ahiremagalur@pivotal.io>
2015-05-29 14:57:57 -0700

    Remove support for OM 1.1 and 1.2
    Talked to Matt Reider, turns out we do not need to support these anymore. Also we do not use these in CI anymore

--- lib/ops_manager_ui_drivers/page_helpers.rb
+++ lib/ops_manager_ui_drivers/page_helpers.rb
@@ -4,14 +4,6 @@ module OpsManagerUiDrivers
   module PageHelpers
     include OpsManagerUiDrivers::WaitHelper
 
-    def om_1_1(ops_manager_url)
-      @om_1_1 ||= create_web_ui(ops_manager_url, Version11)
-    end
-
-    def om_1_2(ops_manager_url)
-      @om_1_2 ||= create_web_ui(ops_manager_url, Version12)
-    end
-
     def om_1_3(ops_manager_url)
       @om_1_3 ||= create_web_ui(ops_manager_url, Version13)
     end
@@ -22,14 +14,6 @@ module OpsManagerUiDrivers
 
     def om_rc(ops_manager_url)
       @om_1_5 ||= create_web_ui(ops_manager_url, Version15)
-    end
-
-    def api_1_1(host:, username:, password:)
-      Version11::Api.new(host: host, user: username, password: password)
-    end
-
-    def api_1_2(host:, username:, password:)
-      Version12::Api.new(host: host, user: username, password: password)
     end
 
     def api_1_3(host:, username:, password:)
