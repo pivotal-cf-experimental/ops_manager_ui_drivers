832c04926a26d8be5b03e7f38b36c7164c235fd3
Alvaro Perez-Shirley <aperezshirley@pivotal.io>
2015-08-07 11:01:25 -0700

    Add ui driver for advanced infrastructure config section on the microbosh
    [#98958820]
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>
    Signed-off-by: Alvaro Perez-Shirley <aperezshirley@pivotal.io>
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

---
+++ lib/ops_manager_ui_drivers/version15/sections/advanced_infrastructure_config.rb
@@ -0,0 +1,17 @@
+module OpsManagerUiDrivers
+  module Version15
+    module Sections
+      class AdvancedInfrastructureConfig
+        def initialize(browser:)
+          @microbosh_form_section = MicroboshFormSection.new(browser, 'advanced_infrastructure_config')
+        end
+
+        def set_connection_options(connection_options)
+          @microbosh_form_section.open_form('advanced_infrastructure_config')
+          @microbosh_form_section.set_fields({'connection_options' => connection_options})
+          @microbosh_form_section.save_form
+        end
+      end
+    end
+  end
+end
---
+++ spec/ops_manager_ui_drivers/version16/sections/advanced_infrastructure_config_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'capybara'
+require 'capybara/rspec/matchers'
+require 'recursive-open-struct'
+
+module OpsManagerUiDrivers
+  module Version16
+    module Sections
+      RSpec.describe AdvancedInfrastructureConfig do
+        class FakeCapybaraExampleGroup < RSpec::Core::ExampleGroup
+          include Capybara::DSL
+          include Capybara::RSpecMatchers
+          include OpsManagerUiDrivers::WaitHelper
+        end
+
+        let(:browser) { instance_double(FakeCapybaraExampleGroup) }
+        let(:microbosh_form_section) { instance_double(MicroboshFormSection) }
+
+        subject(:advanced_infrastructure_config) { AdvancedInfrastructureConfig.new(browser: browser) }
+
+        before do
+          allow(MicroboshFormSection).to receive(:new).with(browser, 'advanced_infrastructure_config').and_return(microbosh_form_section)
+        end
+
+        describe '#set_connection_options' do
+          it 'opens the iaas_configuration, fills out the fields and saves the form' do
+            expect(microbosh_form_section).to receive(:open_form).with('advanced_infrastructure_config')
+            expect(microbosh_form_section).to receive(:set_fields).with({'connection_options' => 'some_text'})
+            expect(microbosh_form_section).to receive(:save_form)
+            advanced_infrastructure_config.set_connection_options('some_text')
+          end
+        end
+      end
+    end
+  end
+end
