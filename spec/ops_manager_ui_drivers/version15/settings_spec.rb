18ed8d87bb51f755d22e692f0d7032ede99c6865
Alvaro Perez-Shirley <aperezshirley@pivotal.io>
2015-08-07 11:42:28 -0700

    Backfill tests for OpsManagerUiDrivers::Version15/16::Settings
    Signed-off-by: Aakash Shah <ashah@pivotal.io>
    Signed-off-by: Alvaro Perez-Shirley <aperezshirley@pivotal.io>

--- spec/ops_manager_ui_drivers/version15/settings_spec.rb
+++ spec/ops_manager_ui_drivers/version15/settings_spec.rb
@@ -6,7 +6,7 @@ module OpsManagerUiDrivers
     RSpec.describe Settings do
       describe 'Openstack settings' do
         let(:settings_hash) { {
-          iaas_type:   'openstack',
+          iaas_type: 'openstack',
           ops_manager: {
             openstack: {
               identity_endpoint: 'IdentityEndpoint',
@@ -36,6 +36,112 @@ module OpsManagerUiDrivers
           expect(fields['key_pair_name']).to eq('KeyPairName')
           expect(fields['ssh_private_key']).to eq('SshPrivateKey')
           expect(fields['disable_dhcp']).to eq(false)
+        end
+      end
+
+      describe 'AWS settings' do
+        let(:settings_hash) do
+          {
+            iaas_type: 'aws',
+            ops_manager: {
+              aws: {
+                aws_access_key: 'AwsAccessKey',
+                aws_secret_key: 'AwsSecretKey',
+                vpc_id: 'VpcId',
+                security_group: 'SecurityGroup',
+                key_pair_name: 'KeyPairName',
+                ssh_key: 'SSHKey',
+                region: 'Region',
+              }
+            }
+          }
+        end
+        let(:test_settings) { RecursiveOpenStruct.new(settings_hash, recurse_over_arrays: true) }
+
+        subject(:aws_settings) { Settings.for(test_settings) }
+
+        it 'should have the correct fields' do
+          fields = aws_settings.fields
+          expect(fields['access_key_id']).to eq('AwsAccessKey')
+          expect(fields['secret_access_key']).to eq('AwsSecretKey')
+          expect(fields['vpc_id']).to eq('VpcId')
+          expect(fields['security_group']).to eq('SecurityGroup')
+          expect(fields['key_pair_name']).to eq('KeyPairName')
+          expect(fields['ssh_private_key']).to eq('SSHKey')
+          expect(fields['region']).to eq('Region')
+        end
+      end
+
+      describe 'vSphere settings' do
+        let(:settings_hash) do
+          {
+            iaas_type: 'vsphere',
+            ops_manager: {
+              vcenter: {
+                creds: {
+                  ip: 'CredsIp',
+                  username: 'Username',
+                  password: 'Password'
+                },
+                datacenter: 'Datacenter',
+                datastore: 'Datastore',
+                microbosh_vm_folder: 'MicroboshVmFolder',
+                microbosh_template_folder: 'MicroboshTemplateFolder',
+                microbosh_disk_path: 'MicroboshDiskPath',
+              }
+            }
+          }
+        end
+        let(:test_settings) { RecursiveOpenStruct.new(settings_hash, recurse_over_arrays: true) }
+
+        subject(:vsphere_settings) { Settings.for(test_settings) }
+
+        it 'should have the correct fields' do
+          fields = vsphere_settings.fields
+          expect(fields['vcenter_ip']).to eq('CredsIp')
+          expect(fields['vcenter_username']).to eq('Username')
+          expect(fields['vcenter_password']).to eq('Password')
+          expect(fields['datacenter']).to eq('Datacenter')
+          expect(fields['datastores_string']).to eq('Datastore')
+          expect(fields['microbosh_vm_folder']).to eq('MicroboshVmFolder')
+          expect(fields['microbosh_template_folder']).to eq('MicroboshTemplateFolder')
+        end
+      end
+
+      describe 'vCloud settings' do
+        let(:settings_hash) do
+          {
+            iaas_type: 'vcloud',
+            ops_manager: {
+              vcloud: {
+                creds: {
+                  url: 'CredsURL',
+                  organization: 'CredsOrganization',
+                  user: 'Username',
+                  password: 'Password'
+                },
+                vdc: {
+                  name: 'VdcName',
+                  storage_profile: 'VdcStorageProfile',
+                  catalog_name: 'VdcCatalogName',
+                },
+              }
+            }
+          }
+        end
+        let(:test_settings) { RecursiveOpenStruct.new(settings_hash, recurse_over_arrays: true) }
+
+        subject(:vcloud_settings) { Settings.for(test_settings) }
+
+        it 'should have the correct fields' do
+          fields = vcloud_settings.fields
+          expect(fields['vcd_url']).to eq('CredsURL')
+          expect(fields['organization']).to eq('CredsOrganization')
+          expect(fields['vcd_username']).to eq('Username')
+          expect(fields['vcd_password']).to eq('Password')
+          expect(fields['datacenter']).to eq('VdcName')
+          expect(fields['storage_profile']).to eq('VdcStorageProfile')
+          expect(fields['catalog_name']).to eq('VdcCatalogName')
         end
       end
     end
