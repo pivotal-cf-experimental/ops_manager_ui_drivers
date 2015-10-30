a95280dc259ad6288e8589cf8b685c45cc9db8cd
David Stevenson <dstevenson@pivotal.io>
2015-10-30 11:24:38 -0700

    PageHelpers can accept `browser` to facilitate usage in rails feature specs
    Signed-off-by: Aram Price <aprice@pivotal.io>

--- lib/ops_manager_ui_drivers/page_helpers.rb
+++ lib/ops_manager_ui_drivers/page_helpers.rb
@@ -4,20 +4,36 @@ module OpsManagerUiDrivers
   module PageHelpers
     include OpsManagerUiDrivers::WaitHelper
 
-    def om_1_4(ops_manager_url)
-      @om_1_4 ||= create_web_ui(ops_manager_url, Version14)
+    def om_1_4(ops_manager_url, browser = self)
+      @om_1_4 ||= create_web_ui(
+        ops_manager_url: ops_manager_url,
+        browser: browser,
+        version_module: Version14,
+      )
     end
 
-    def om_1_5(ops_manager_url)
-      @om_1_5 ||= create_web_ui(ops_manager_url, Version15)
+    def om_1_5(ops_manager_url, browser = self)
+      @om_1_5 ||= create_web_ui(
+        ops_manager_url: ops_manager_url,
+        browser: browser,
+        version_module: Version15,
+      )
     end
 
-    def om_1_6(ops_manager_url)
-      @om_1_6 ||= create_web_ui(ops_manager_url, Version16)
+    def om_1_6(ops_manager_url, browser = self)
+      @om_1_6 ||= create_web_ui(
+        ops_manager_url: ops_manager_url,
+        browser: browser,
+        version_module: Version16,
+      )
     end
 
-    def om_rc(ops_manager_url)
-      @om_1_7 ||= create_web_ui(ops_manager_url, Version17)
+    def om_rc(ops_manager_url, browser = self)
+      @om_1_7 ||= create_web_ui(
+        ops_manager_url: ops_manager_url,
+        browser: browser,
+        version_module: Version17,
+      )
     end
 
     def api_1_4(host:, username:, password:)
@@ -38,10 +54,10 @@ module OpsManagerUiDrivers
 
     private
 
-    def create_web_ui(ops_manager_url, version_module)
+    def create_web_ui(ops_manager_url:, browser:, version_module:)
       Capybara.app_host = ops_manager_url
       puts "Preparing to interact with #{version_module.inspect} at #{Capybara.app_host}"
-      version_module::WebUi.new(browser: self)
+      version_module::WebUi.new(browser: browser)
     end
   end
 end
