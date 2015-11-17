b50cfb8f3c6d38045c002ceb1b7f63eb1ecb6cd9
Caroline Taymor <ctaymor@pivotal.io>
2015-11-17 11:42:48 -0800

    Adds support for delete_unused_products link
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/product_dashboard.rb
+++ lib/ops_manager_ui_drivers/version17/product_dashboard.rb
@@ -3,6 +3,10 @@ require 'ops_manager_ui_drivers/version16/product_dashboard'
 module OpsManagerUiDrivers
   module Version17
     class ProductDashboard < Version16::ProductDashboard
+      def delete_unused_products
+        open_dashboard
+        @browser.find('a#delete_unused_products').trigger('click')
+      end
     end
   end
 end
--- spec/ops_manager_ui_drivers/version17/product_dashboard_spec.rb
+++ spec/ops_manager_ui_drivers/version17/product_dashboard_spec.rb
@@ -4,14 +4,13 @@ require 'capybara'
 module OpsManagerUiDrivers
   module Version17
     describe ProductDashboard do
+      let(:browser) { double('browser') }
+      let(:product_dashboard) { OpsManagerUiDrivers::Version17::ProductDashboard.new(browser: browser) }
+      before do
+        allow(browser).to receive(:visit)
+      end
+
       describe '#product_complete?' do
-        let(:product_dashboard) { OpsManagerUiDrivers::Version17::ProductDashboard.new(browser: browser) }
-        let(:browser) { double('browser') }
-
-        before do
-          allow(browser).to receive(:visit)
-        end
-
         it 'returns true when complete' do
           expect(browser).to receive(:all).with("a#show-p-bosh-configure-action[data-progress='100']").and_return([instance_double(Capybara::Node)])
           expect(product_dashboard.product_complete?('p-bosh')).to eq(true)
@@ -20,6 +19,18 @@ module OpsManagerUiDrivers
         it 'returns false when incomplete' do
           expect(browser).to receive(:all).with("a#show-p-bosh-configure-action[data-progress='100']").and_return([])
           expect(product_dashboard.product_complete?('p-bosh')).to eq(false)
+        end
+      end
+
+      describe '#delete_unused_products' do
+        it 'clicks on the delete_unused_products link' do
+          delete_products_link = double('delete products link')
+          allow(delete_products_link).to receive(:trigger)
+          allow(browser).to receive(:find).with('a#delete_unused_products').and_return(delete_products_link)
+
+          product_dashboard.delete_unused_products
+
+          expect(delete_products_link).to have_received(:trigger).with('click')
         end
       end
     end
