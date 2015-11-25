71ac05a07e80fa93fd7995aed410cd227882d4eb
Joseph Palermo <jpalermo@pivotal.io>
2015-11-25 10:33:34 -0800

    Support new structure of network page; Remove worthless test
    Signed-off-by: David Stevenson <dstevenson@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/bosh_product_sections/networks.rb
+++ lib/ops_manager_ui_drivers/version17/bosh_product_sections/networks.rb
@@ -13,12 +13,12 @@ module OpsManagerUiDrivers
         def add_network(name:, iaas_network_identifier:, subnet:, dns:, gateway:, reserved_ip_ranges:)
           @bosh_product_form_section.open_form('network')
 
-          @browser.click_on 'Add'
-          @bosh_product_form_section.set_fields(
-            'name' => name,
-          )
+          @browser.click_on 'Add Network'
+          @bosh_product_form_section.set_fields('name' => name)
+
           subnet_section = Subnet.new(browser: @browser, network_form: @bosh_product_form_section)
           subnet_section.add_subnet(iaas_identifier: iaas_network_identifier, subnet: subnet, dns: dns, gateway: gateway, reserved_ip_ranges: reserved_ip_ranges)
+
           @browser.click_on 'Save'
           @browser.expect(@browser.page).to @browser.have_css(FLASH_MESSAGE_CLASS)
           flash_errors = @browser.all(FLASH_MESSAGE_ERRORS).to_a
@@ -31,8 +31,9 @@ module OpsManagerUiDrivers
 
         def delete_network(network_name)
           @bosh_product_form_section.open_form('network')
-          button = @browser.find("i[id='delete-#{network_name}']")
-          button.trigger('click')
+          button = @browser.find('button', text: network_name)
+          button.click
+          button.find(:xpath, '..').first('.with-delete-record').trigger('click')
 
           @bosh_product_form_section.save_form
         end
--- spec/ops_manager_ui_drivers/version17/bosh_product_sections/networks_spec.rb
+++ spec/ops_manager_ui_drivers/version17/bosh_product_sections/networks_spec.rb
@@ -16,7 +16,6 @@ module OpsManagerUiDrivers
         let(:browser) { instance_double(FakeCapybaraExampleGroup) }
         let(:expectation_target) { instance_double(RSpec::Expectations::ExpectationTarget, to: nil) }
         let(:field_node) { instance_double(Capybara::Node::Element, set: nil) }
-        let(:delete_button) { double('delete button') }
         let(:page) { double(:page) }
         let(:flash_selector) {double(:flash_selector)}
 
@@ -29,7 +28,6 @@ module OpsManagerUiDrivers
           allow(browser).to receive(:page)
           allow(browser).to receive(:click_on)
           allow(browser).to receive(:have_css)
-          allow(browser).to receive(:find).and_return(delete_button)
           allow(browser).to receive(:page).and_return(page)
           allow(browser).to receive(:have_css).with('.flash-message.success').and_return(flash_selector)
           allow(browser).to receive(:have_css).with('.flash-message').and_return(flash_selector)
@@ -83,7 +81,7 @@ module OpsManagerUiDrivers
             expect(browser).to have_received(:visit).with('/').ordered
             expect(browser).to have_received(:click_on).with('show-p-bosh-configure-action').ordered
             expect(browser).to have_received(:click_on).with('show-network-action').ordered
-            expect(browser).to have_received(:click_on).with('Add').ordered
+            expect(browser).to have_received(:click_on).with('Add Network').ordered
 
             expect(browser).to have_received(:all).with(:field, 'network_collection[networks_attributes][0][name]', minimum: 1).ordered
             expect(field_node).to have_received(:set).with('Name').ordered
@@ -102,25 +100,6 @@ module OpsManagerUiDrivers
 
             expect(browser).to have_received(:all).with(:field, 'network_collection[networks_attributes][0][subnets][0][reserved_ip_ranges]', minimum: 1).ordered
             expect(field_node).to have_received(:set).with('ReservedIpRanges').ordered
-
-            expect(browser).to have_received(:click_on).with('Save').ordered
-            expect(browser).to have_received(:expect).with(page).ordered
-            expect(expectation_target).to have_received(:to).with(flash_selector).ordered
-          end
-        end
-
-        describe '#delete_network' do
-          before do
-            allow(delete_button).to receive(:trigger).with('click')
-          end
-
-          it 'navigates to the root-page > p-bosh configuration > configure networks page > and deletes the specified network' do
-            networks.delete_network('Name')
-
-            expect(browser).to have_received(:visit).with('/').ordered
-            expect(browser).to have_received(:click_on).with('show-p-bosh-configure-action').ordered
-            expect(browser).to have_received(:click_on).with('show-network-action').ordered
-            expect(browser).to have_received(:find).with("i[id='delete-Name']").ordered
 
             expect(browser).to have_received(:click_on).with('Save').ordered
             expect(browser).to have_received(:expect).with(page).ordered
