2abe6901a767547980f2150c850e732b19010cb2
Ben Watts <bwatts@pivotal.io>
2015-12-03 11:14:30 -0800

    Drivers can no set persistent disk type
    * remove suppport for `#set_resources_for_jobs`
       - use individual methods:
         `set_ephemeral_disk_for_job`
         `set_disk_type_for_job`
         `set_instances_for_job`
    
    [#108398204]
    
    Signed-off-by: Aram Price <aprice@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/product_resource_configuration.rb
+++ lib/ops_manager_ui_drivers/version17/product_resource_configuration.rb
@@ -8,46 +8,53 @@ module OpsManagerUiDrivers
         @product_name = product_name
       end
 
-      def set_instances_for_job(job_name, instance_count)
-        open_form
-        browser.find_field("product_resources_form[#{job_name}][instances][value]").set(instance_count)
-        save_form
+      def set_instances_for_job(job_name, instance_count, validate: true)
+        set_resources_for_jobs(job_name, :instances, instance_count, validate: validate)
       end
 
-      def set_instance_type_for_job(job_name, instance_type)
+      def set_ephemeral_disk_for_job(job_name, ephemeral_disk, validate: true)
+        set_resources_for_jobs(job_name, :ephemeral_disk, ephemeral_disk, validate: validate)
+      end
+
+      def set_instance_type_for_job(job_name, instance_type, validate: true)
         open_form
         browser.
           find("select[name='product_resources_form[#{job_name}][instance_type_id]']").
           find("option[value='#{instance_type}']").
           select_option
-        save_form
+        save_form(validate: validate)
       end
 
-      def set_elb_names_for_job(job_name, comma_delimited_elb_names)
+      def set_disk_type_for_job(job_name, disk_type_id, validate: true)
+        open_form
+        browser.
+          find("select[name='product_resources_form[#{job_name}][disk_type_id]']").
+          find("option[value='#{disk_type_id}']").
+          select_option
+        save_form(validate: validate)
+      end
+
+      def set_elb_names_for_job(job_name, comma_delimited_elb_names, validate: true)
         open_form
         browser.find_field("product_resources_form[#{job_name}][elb_names]").set(comma_delimited_elb_names)
-        save_form
+        save_form(validate: validate)
       end
 
-      def set_floating_ips_for_job(job_name, comma_delimited_floating_ips)
+      def set_floating_ips_for_job(job_name, comma_delimited_floating_ips, validate: true)
         open_form
         browser.find_field("product_resources_form[#{job_name}][floating_ips]").set(comma_delimited_floating_ips)
-        save_form
-      end
-
-      def set_resources_for_jobs(resources_by_job, validate: true)
-        open_form
-        resources_by_job.each do |job, resources|
-          resources.each do |resource_or_instance, value|
-            browser.fill_in("product_resources_form[#{job}][#{resource_or_instance}][value]", with: value)
-          end
-        end
         save_form(validate: validate)
       end
 
       private
 
       attr_reader :browser
+
+      def set_resources_for_jobs(job_name, resource, value, validate: true)
+        open_form
+        browser.fill_in("product_resources_form[#{job_name}][#{resource}][value]", with: value)
+        save_form(validate: validate)
+      end
 
       def open_form
         browser.visit '/'
