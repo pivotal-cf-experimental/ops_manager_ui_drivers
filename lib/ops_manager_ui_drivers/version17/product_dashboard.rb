bec6b64af4d8be75e7aba9ffc14df5607b4bc613
Maryam Labib <mlabib@pivotal.io>
2015-12-22 11:02:50 -0800

    Fix #component_version renamed to #product_version in product dashboard
    Signed-off-by: David Stevenson <dstevenson@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/product_dashboard.rb
+++ lib/ops_manager_ui_drivers/version17/product_dashboard.rb
@@ -3,6 +3,11 @@ require 'ops_manager_ui_drivers/version16/product_dashboard'
 module OpsManagerUiDrivers
   module Version17
     class ProductDashboard < Version16::ProductDashboard
+      def product_available?(product_name, product_version)
+        open_dashboard
+        browser.all("li.#{product_name} input#product_version[value='#{product_version}']", {visible: false}).any?
+      end
+
       def delete_unused_products
         open_dashboard
         @browser.find('a#delete_unused_products').trigger('click')
