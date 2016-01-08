f5cb17b66831a0da5f74d2447f1aa256b838d5e4
Caroline Taymor <ctaymor@pivotal.io>
2016-01-08 10:45:42 -0800

    Import flow has changed in ops manager 1.7
    * Operator can now chose between setup and importing
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/product_dashboard.rb
+++ lib/ops_manager_ui_drivers/version17/product_dashboard.rb
@@ -3,6 +3,16 @@ require 'ops_manager_ui_drivers/version16/product_dashboard'
 module OpsManagerUiDrivers
   module Version17
     class ProductDashboard < Version16::ProductDashboard
+      def import_installation_file(file_path, decryption_passphrase)
+        browser.visit '/import/new'
+
+        browser.fill_in 'import[passphrase]', with: decryption_passphrase
+        browser.attach_file 'import[file]', file_path
+        browser.click_on 'Import'
+
+        browser.poll_up_to_times(20) { browser.assert_text('Successfully imported installation.') }
+      end
+
       def product_available?(product_name, product_version)
         open_dashboard
         browser.all("li.#{product_name} input#product_version[value='#{product_version}']", {visible: false}).any?
