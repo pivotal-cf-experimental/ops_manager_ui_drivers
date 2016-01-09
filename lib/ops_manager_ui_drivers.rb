be8326621e1c250735ce46416c76e61ea3d3810c
Derek Richard <drichard@pivotal.io>
2016-01-08 16:18:19 -0800

    fix load order
    Signed-off-by: Ben Watts <bwatts@pivotal.io>

--- lib/ops_manager_ui_drivers.rb
+++ lib/ops_manager_ui_drivers.rb
@@ -1,5 +1,7 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'backport_refinements'
+
 Dir[File.join(__dir__, '**', '*.rb')].each do |ops_manager_ui_drivers|
   require ops_manager_ui_drivers
 end
