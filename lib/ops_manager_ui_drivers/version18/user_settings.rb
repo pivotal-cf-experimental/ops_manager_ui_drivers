801ac1d70de38de3119d0a826c94206ad9291ab7
Matt Royal <mroyal@pivotal.io>
2016-03-21 12:09:53 -0700

    The "Troubleshooting" page is now the "Advanced" page with a new route

--- lib/ops_manager_ui_drivers/version18/user_settings.rb
+++ lib/ops_manager_ui_drivers/version18/user_settings.rb
@@ -2,13 +2,13 @@ module OpsManagerUiDrivers
   module Version18
     class UserSettings < Version17::UserSettings
       def delete_whole_installation_on_next_apply_updates
-        browser.visit '/troubleshooting'
+        browser.visit '/advanced_tools'
         browser.click_on 'show-delete-installation-modal-action'
         browser.click_on 'delete-installation-action'
       end
 
       def delete_installation_available?
-        browser.visit '/troubleshooting'
+        browser.visit '/advanced_tools'
         browser.all('#show-delete-installation-modal-action').any?
       end
     end
