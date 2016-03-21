04399a0ccb18ffe8bae24df7dd4423e2351dcc79
Scott Muc <smuc@pivotal.io>
2016-03-21 11:26:23 -0700

    Move methods for deleting the installation to Version18::UserSettings
    Deleting the installation is now done through a different page.
    
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

--- lib/ops_manager_ui_drivers/version18/product_dashboard.rb
+++ lib/ops_manager_ui_drivers/version18/product_dashboard.rb
@@ -1,6 +1,19 @@
 module OpsManagerUiDrivers
   module Version18
     class ProductDashboard < Version17::ProductDashboard
+      def delete_whole_installation
+        method_deprecated!
+      end
+
+      def delete_installation_available?
+        method_deprecated!
+      end
+
+      private
+
+      def method_deprecated!
+        raise NotImplementedError, 'This method has been removed. You can find the new version on the UserSettings class'
+      end
     end
   end
 end
--- lib/ops_manager_ui_drivers/version18/user_settings.rb
+++ lib/ops_manager_ui_drivers/version18/user_settings.rb
@@ -1,6 +1,16 @@
 module OpsManagerUiDrivers
   module Version18
     class UserSettings < Version17::UserSettings
+      def delete_whole_installation_on_next_apply_updates
+        browser.visit '/troubleshooting'
+        browser.click_on 'show-delete-installation-modal-action'
+        browser.click_on 'delete-installation-action'
+      end
+
+      def delete_installation_available?
+        browser.visit '/troubleshooting'
+        browser.all('#show-delete-installation-modal-action').any?
+      end
     end
   end
 end
