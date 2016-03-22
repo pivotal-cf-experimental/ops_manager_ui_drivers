6d32914fbaa14470d060ab923d80c52103fada53
Matt Royal <mroyal@pivotal.io>
2016-03-22 12:16:12 -0700

    Hover over add product link to make button visible
    Signed-off-by: Andrew Cantino <acantino@pivotal.io>

--- lib/ops_manager_ui_drivers/version17/available_products.rb
+++ lib/ops_manager_ui_drivers/version17/available_products.rb
@@ -3,18 +3,15 @@ require 'ops_manager_ui_drivers/wait_helper'
 module OpsManagerUiDrivers
   module Version17
     class AvailableProducts
-      include WaitHelper
-
       def initialize(browser:)
         @browser = browser
       end
 
       def add_product_to_install(product_name)
         browser.visit '/'
-        poll_up_to_times 3 do
-          browser.click_on "add-#{product_name}"
-          browser.find("#show-#{product_name}-configure-action")
-        end
+        browser.find("ul.product-list li.#{product_name}.product").hover
+        browser.click_on "add-#{product_name}"
+        browser.find("#show-#{product_name}-configure-action")
       end
 
       def product_added?(product_name)
