414580b0c2073621a537218d959639ef6b18affc
Wagner Camarao <wcamarao@vmware.com>
2016-04-08 12:15:21 -0700

    Override StateChangeProgress#errand_ran? for 1.8
    - The errand name is now the guid of the job, so we needed to update the matcher
    
    [#114742413]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- lib/ops_manager_ui_drivers/version18/state_change_progress.rb
+++ lib/ops_manager_ui_drivers/version18/state_change_progress.rb
@@ -3,6 +3,11 @@ require 'ops_manager_ui_drivers/version17/state_change_progress'
 module OpsManagerUiDrivers
   module Version18
     class StateChangeProgress < OpsManagerUiDrivers::Version17::StateChangeProgress
+      def errand_ran?(errand_name)
+        open_install_progress
+        !!(/Errand `#{errand_name}-\w+' completed successfully \(exit code 0\)/ =~
+          browser.find('#install-output .output', visible: false).text(:all))
+      end
     end
   end
 end
