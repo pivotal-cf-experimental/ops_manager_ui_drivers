b7955f24d89ccdb37d3062928bc217c647bfb276
Benjamin Gröhbiel <benjamin.groehbiel@gmail.com>
2016-10-06 00:39:26 +0100

    Service Network checkbox can be ticked
    The network configuration of an environment, can contain a property
    'is_service_network' which will according to its value check or
    uncheck the service network checkbox.
    
    If 'is_service_network' is false or absent, the service network
    checkbox will not be checked.
    
    If 'is_service_network' is true, the checkbox will be checked.

--- lib/ops_manager_ui_drivers/version17/ops_manager_director.rb
+++ lib/ops_manager_ui_drivers/version17/ops_manager_director.rb
@@ -52,6 +52,7 @@ module OpsManagerUiDrivers
         iaas_networks && iaas_networks.each do |network|
           networks.add_network(
             name: network['name'],
+            is_service_network: network.fetch("is_service_network", false),
             subnets: network['subnets'],
           )
         end
